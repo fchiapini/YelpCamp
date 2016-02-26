232d0744c72dea37e08e9f430138dba4ef3a9706
Felipe <fchiapini@gmail.com>
2016-02-26 15:02:38 -0300

    Added styling to campgrounds page

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.set("view engine", "ejs");
 var campgrounds = [
 		{name: "Salmon Creek", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
 		{name: "Granite Hill", image: "https://farm6.staticflickr.com/5098/5496185186_d7d7fed22a.jpg"},
+		{name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+		{name: "Salmon Creek", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
+		{name: "Granite Hill", image: "https://farm6.staticflickr.com/5098/5496185186_d7d7fed22a.jpg"},
+		{name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+		{name: "Salmon Creek", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
+		{name: "Granite Hill", image: "https://farm6.staticflickr.com/5098/5496185186_d7d7fed22a.jpg"},
+		{name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+		{name: "Salmon Creek", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
+		{name: "Granite Hill", image: "https://farm6.staticflickr.com/5098/5496185186_d7d7fed22a.jpg"},
 		{name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"}
 ];
 
