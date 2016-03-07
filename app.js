6b51251ec73cbcafbaee61c269b4a019ff529a83
Felipe <fchiapini@gmail.com>
2016-03-07 11:22:24 -0300

    Added mongolab

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var express          = require("express"),
     flash            = require("connect-flash"),
     app              = express()
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://felipe:felipe@ds023458.mlab.com:23458/camp_database");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
